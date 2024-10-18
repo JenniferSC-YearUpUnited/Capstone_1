eeab1dc6fb16aa2d21cc833fdf493aeb37ea792c
Student <jsantoscustodio@appdev.yearup.org>
2024-10-17 22:28:06 -0500

    Adding sample data

--- business_operations.sql
+++ business_operations.sql
@@ -1,6 +1,8 @@
+## CREATE DATA BASE FIRST ##
 CREATE DATABASE jensprinklespectacles;
 USE jensprinklespectacles;
 
+## CREATE FIRST TABLE WITH PRIMARY KEY  ## 
 CREATE TABLE customers (
 customerid INT PRIMARY KEY AUTO_INCREMENT, 
 firstname VARCHAR(100),
@@ -10,8 +12,10 @@ zipcode VARCHAR(5),
 phonenumber VARCHAR(10)
 );
 
+## SEE ALL COLUMNS FOR TABLE1 ## 
 SELECT * FROM customers;
 
+## CREATE TABLE2 WITH PRIMARY KEY## 
 CREATE TABLE purchases (
 purchaseid INT PRIMARY KEY AUTO_INCREMENT,
 customerid INT,
@@ -20,18 +24,21 @@ scentid INT,
 bags INT
 );
 
+## ADDING FOREIGN KEY 1 CONSTRAINT AFTER CREATING TABLE 1##
 ALTER TABLE purchases
 ADD CONSTRAINT fk1 FOREIGN KEY (customerid)
 REFERENCES customers(customerid);
-
+## ADDING FOREIGN KEY 2 CONSTRAINT AFTER CREATING TABLE 3## 
 ALTER TABLE purchases
 ADD CONSTRAINT fk2 FOREIGN KEY (scentid)
 REFERENCES scents(scentid);
 
+## SEE ALL COLUMNS FOR TABLE2##
 SELECT * FROM purchases;
 
+## CREATE TABLE3 WITH PRIMARY KEY##
 CREATE TABLE scents (
-scentid INT PRIMARY KEY AUTO_INCREMENT,
+scentid INT PRIMARY KEY AUTO_INCREMENT, 
 scentname VARCHAR(100),
 cartonprice DECIMAL(5,2),
 cartonsinstock VARCHAR(20),
@@ -40,4 +47,8 @@ cartonsonorder SMALLINT,
 discontinuedcartons SMALLINT
 );
 
+
+
+## SEE ALL COLUMNS FOR TABLE3 ## 
 SELECT * FROM scents;
+
---
+++ sample_data.sql
@@ -0,0 +1,48 @@
+### ADDING 1ST RECORD TO TABLE1 ##
+
+INSERT INTO customers 
+VALUES (1,"Katherine", "Cooper", "Chicago", "60646", "3124444949");
+
+## ADDING THE REST OF THE RECORDS TO TABLE1 ##
+INSERT INTO customer (firstname, lastname, city, zipcode, phonenumber)
+VALUES ("Steven", "Morris", "Chicago", "60647", "3126666161"),
+( "Rachel", "Peterson", "Chicago", "60648", "3128888383"),
+( "Aron", "Howard", "Chicago", "60649", "3120000606"),
+( "Laura", "King", "Chicago", "60650", "3121111616");
+
+## SEE ALL COLUMNS## 
+SELECT * FROM customers;
+
+## ADDING 1ST RECORD TO TABLE3 ## 
+INSERT INTO scents
+VALUES ("1", "Ocean Breeze", 25.99, "8", 5, 0, 0);
+
+## ADDING THE REST OF THE RECORDS TO TABLE3## 
+INSERT INTO scents (scentname, cartonprice, cartonsinstock, reorderlevel, cartonsonorder, discontinuedcartons)
+VALUES ("Vanilla Vibes", 22.49, "12", 6, 0, 0),
+("Summer Romance", 18.75, "3", 5, 9, 0),
+("Happy Citrus", 21.99, "0", 0, 0, 1),
+("Sunshine Breeze", 24.50, "15", 6, 2, 0);  
+
+## SEE ALL COLUMNS ##
+SELECT * FROM scents;
+
+
+## ADDING RECORDS TO PURCHASES ## 
+INSERT INTO purchases (purchaseid, customerid, purchasedate,scentid,bags)
+VALUES (1, 1, "2024-09-01", 2, 4);
+
+INSERT INTO purchases (customerid, purchasedate,scentid,bags)
+VALUES (3, "2024-09-18", 4, 1);
+
+INSERT INTO purchases (customerid, purchasedate,scentid,bags)
+VALUES (1, "2024-10-02", 1, 2),
+(5, "2024-10-08", 5, 3),
+(2, "2024-10-11", 3, 5);
+
+## SEE ALL COLUMNS ## 
+SELECT * FROM purchases;
+
+
+
+
